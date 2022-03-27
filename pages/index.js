290b8c291f7097699803435ca2a95da1c672521c
scopionjs <josephsiame233@gmail.com>
2022-03-27 15:17:20 -0700

    3 files modified

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,12 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Create Next  yyApp</title>
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome uuiii to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
